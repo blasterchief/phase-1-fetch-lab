bd072e76d8d3437ad986a233f1c535b19fa8f4a7
Conrad Etherington <conrad.etherington@gmail.com>
2022-10-17 11:16:35 -0600

    commit tree 82d3e71fd4cabaa2932f0fb86f6176ce10764b0d

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+return fetch("https://anapioficeandfire.com/api/books")
+
+ .then((response) => {
+  return response.json()
+
+ })
+ .then((json) => {
+  console.log(json)
+ renderBooks(json)
+ })
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+  const main = document.querySelector('main'); //identify DOM
+  books.forEach(book => {  //iterate over all books
+    const h2 = document.createElement('h2');  //create element
+    const pisbn = document.createElement('p');  //create element
+    h2.innerText = book.name;  
+    pisbn.innerText = book.isbn;
+    const pcharacters = document.createElement('p'); //create element
+    pcharacters.innerText = book.characters;
+    h2.appendChild(pcharacters);
     main.appendChild(h2);
+    h2.appendChild(pisbn);
   });
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks(); {}
 });
